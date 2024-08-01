bcb9c08876f2fc43fd12fcc0a38b8b052fb8d6af
Srikanth K <kolleparasrikanth@gmail.com>
2024-08-01 18:51:27 +0530

    Side Drawer and NavLinks, button to pages are handled

--- src/shared/components/Navigation/MainNavigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation/MainNavigation.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MainNavigation.css";
 import MainHeader from "../MainHeader/MainHeader.js";
 import { Link } from "react-router-dom";
+import NavLinks from "../NavLinks/NavLinks.js";
+import SideDrawer from "../SideDrawer/SideDrawer.js";
+import BackDrop from "../../UIElements/BackDrop/BackDrop.js";
 
 function MainNavigation() {
+  const [drawerisOpen, setDrawerisOpen] = useState(false);
+
+  const toggleDrawerHandler = () => {
+    setDrawerisOpen(!drawerisOpen);
+  };
+
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span></span> <span></span> <span></span>
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">Your Places</Link>
-      </h1>
-      <nav>...</nav>
-    </MainHeader>
+    <>
+      {drawerisOpen && <BackDrop onClick={toggleDrawerHandler}/>}
+        <SideDrawer show = {drawerisOpen} close = {toggleDrawerHandler}>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      
+
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={toggleDrawerHandler}>
+          <span></span> <span></span> <span></span>
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Your Places</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks></NavLinks>
+        </nav>
+      </MainHeader>
+    </>
   );
 }
 
---
+++ src/shared/components/Navigation/NavLinks/NavLinks.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./NavLinks.css"
+import { NavLink } from 'react-router-dom';
+
+function NavLinks() {
+  return (
+    <ul className='nav-links'>
+        <li>
+            <NavLink to="/" exact>All Users</NavLink>
+        </li>
+        <li>
+            <NavLink to="/1/places">My Places</NavLink>
+        </li>
+        <li>
+            <NavLink to="/places/new">Add Places</NavLink>
+        </li>
+        <li>
+            <NavLink to="/auth">Authenticate</NavLink>
+        </li>
+    </ul>
+  )
+}
+
+export default NavLinks
