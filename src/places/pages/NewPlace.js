61a3fd38c0f68aef6620f84f1cedbe2d4b414405
Srikanth K <kolleparasrikanth@gmail.com>
2024-08-02 15:29:32 +0530

    implemented Validators for adding new places for input element

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./NewPlace.css";
 import Input from "../../shared/components/ForElements/Input/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
 
 function NewPlace() {
   return (
     <form className="place-form">
-      <Input element = "input" type="text" label="Title" />
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid Title!!"
+      />
     </form>
   );
 }
