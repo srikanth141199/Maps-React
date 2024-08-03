f636d27e3d652eb7f22bb3a383094dc32db79a42
Srikanth K <kolleparasrikanth@gmail.com>
2024-08-03 13:11:57 +0530

    Implemented Auth Context to manage Login and Logout

--- src/App.js
+++ src/App.js
@@ -10,22 +10,34 @@ import MainNavigation from "./shared/components/Navigation/MainNavigation/MainNa
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace/UpdatePlace";
 import Auth from "./user/pages/Auth/Auth";
+import { AuthContext } from "./shared/context/auth-context";
+import { useCallback, useState } from "react";
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] =  useState(false);
+  const login = useCallback(()=>{setIsLoggedIn(true)}, []);
+  const logout = useCallback(()=>{setIsLoggedIn(false)},[])
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Routes>
-          <Route path="/" element={<Users />}></Route>
-          <Route path="/:userId/places" element={<UserPlaces />} exact></Route>
-          <Route path="/places/new" element={<NewPlace />}></Route>
-          <Route path="/places/:placeId" element={<UpdatePlace />}></Route>
-          <Route path="/auth" element={<Auth/>}></Route>
-          <Route path="*" element={<Navigate to="/" />}></Route>
-        </Routes>
-      </main>
-    </Router>
+    <AuthContext.Provider value = {{isLoggedIn : isLoggedIn, login : login, logout : logout}}>
+      <Router>
+        <MainNavigation />
+        <main>
+          <Routes>
+            <Route path="/" element={<Users />}></Route>
+            <Route
+              path="/:userId/places"
+              element={<UserPlaces />}
+              exact
+            ></Route>
+            <Route path="/places/new" element={<NewPlace />}></Route>
+            <Route path="/places/:placeId" element={<UpdatePlace />}></Route>
+            <Route path="/auth" element={<Auth />}></Route>
+            <Route path="*" element={<Navigate to="/" />}></Route>
+          </Routes>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
