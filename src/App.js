d274000f27c9fdb99e08e9b1fa32920084c2c201
Srikanth K <kolleparasrikanth@gmail.com>
2024-08-22 15:32:31 +0530

    Implemented Lazy Loading

--- src/App.js
+++ src/App.js
@@ -4,21 +4,30 @@ import {
   Routes,
   Navigate,
 } from "react-router-dom";
-import Users from "./user/pages/Users";
-import NewPlace from "./places/pages/NewPlace";
+import React, { Suspense } from "react";
+//import Users from "./user/pages/Users";
+//import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation/MainNavigation";
-import UserPlaces from "./places/pages/UserPlaces";
-import UpdatePlace from "./places/pages/UpdatePlace/UpdatePlace";
-import Auth from "./user/pages/Auth/Auth";
+//import UserPlaces from "./places/pages/UserPlaces";
+//import UpdatePlace from "./places/pages/UpdatePlace/UpdatePlace";
+//import Auth from "./user/pages/Auth/Auth";
 import { AuthContext } from "./shared/context/auth-context";
 import { useAuth } from "./shared/hooks/auth-hook";
+import LoadingSpinner from "./shared/components/UIElements/LoadingSpinner/LoadingSpinner";
+
+const Users = React.lazy(() => import("./user/pages/Users"));
+const NewPlace = React.lazy(() => import("./places/pages/NewPlace"));
+const UserPlaces = React.lazy(() => import("./places/pages/UserPlaces"));
+const UpdatePlace = React.lazy(() =>
+  import("./places/pages/UpdatePlace/UpdatePlace")
+);
+const Auth = React.lazy(() => import("./user/pages/Auth/Auth"));
 
 function App() {
-
   console.log(process.env.REACT_APP_BACKEND_URL);
   console.log(process.env.REACT_APP_GOOGLE_API_KEY);
 
-  const {token, login, logout, userId}  = useAuth();
+  const { token, login, logout, userId } = useAuth();
 
   let routes;
 
@@ -56,7 +65,15 @@ function App() {
       <Router>
         <MainNavigation />
         <main>
-          <Routes>{routes}</Routes>
+          <Suspense
+            fallback={
+              <div className="center">
+                <LoadingSpinner />
+              </div>
+            }
+          >
+            <Routes>{routes}</Routes>
+          </Suspense>
         </main>
       </Router>
     </AuthContext.Provider>
